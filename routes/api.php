c6af1b54e421bcd4412fd0fc6913bb4d4077d214
cipalipampam <agungalamsyah719@gmail.com>
2024-10-24 14:11:09 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ForgotPasswordController;
+use GuzzleHttp\Psr7\Uri;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\apiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/sign_up', [apiController::class, 'sign_up']);
+Route::match(['get', 'post'], '/sign_in', [apiController::class, 'sign_in']);
+Route::post('/forgot_password', [apiController::class, 'forgot_password']);
+Route::post('/reset_password', [apiController::class, 'reset_password']);
